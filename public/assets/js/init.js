38df154f037f6567853e003b0f743e62e6de4af4
Alvin Manansala <aomanansala@gmail.com>
2017-07-30 15:58:13 +0800

    export translated texts into image - landscape orientation

--- public/assets/js/init.js
+++ public/assets/js/init.js
@@ -15,9 +15,12 @@ $(function() {
 
         words.forEach(function(word) {
             if ($('#portrait').is(':checked')) {
-                var html = "<div id='wordWrapper" + wordCounter + "' class='wordWrapper'></div>";
-                $('#translatedContent').append(html);
+                var html = "<div id='wordWrapper" + wordCounter + "' class='wordWrapper portrait'></div>";
+            } else if ($('#landscape').is(':checked')) {
+                var html = "<div id='wordWrapper" + wordCounter + "' class='wordWrapper landscape'></div>";
             }
+
+            $('#translatedContent').append(html);
 
             var _syllables = syllables(word);
      
@@ -25,7 +28,7 @@ $(function() {
                 var firstTwoCharacters = syllable.charAt(0) + syllable.charAt(1);
 
                 if ($('#landscape').is(':checked')) {
-                    landscapeTranslation(syllable, firstTwoCharacters);
+                    landscapeTranslation(syllable, firstTwoCharacters, wordCounter);
                 } else if ($('#portrait').is(':checked')) {
                     portraitTranslation(syllable, firstTwoCharacters, wordCounter);
                 }
@@ -49,25 +52,38 @@ $(function() {
         delete link;
     }); 
 
-    function landscapeTranslation(syllable, firstTwoCharacters) {
+    function landscapeTranslation(syllable, firstTwoCharacters, wordCounter) {
         if ((/[aeiou]/g).test(firstTwoCharacters) == false) {
             if (syllable.length == 3) {
                 var firstCharacter = syllable.charAt(0);
                 var lastTwoCharacters = syllable.charAt(1) + syllable.charAt(2);
 
-                image = "<img src='" + IMAGE_PATH + firstCharacter + IMAGE_EXTENSION + "'>";
-                $('#translatedContent').append(image);
+                image1 = "<img class='image' src='" + IMAGE_PATH + firstCharacter + IMAGE_EXTENSION + "'>";
+                image2 = "<img class='image' src='" + IMAGE_PATH + lastTwoCharacters + IMAGE_EXTENSION + "'>";
 
-                image = "<img src='" + IMAGE_PATH + lastTwoCharacters + IMAGE_EXTENSION + "'>";
-                $('#translatedContent').append(image);
+                var word = $('#wordWrapper' + wordCounter).append("<div class='syllable'>" + image1 + image2 + "</div>");
+
+                $('#translatedContent').append(word);
             }
         } else {
             var str = syllable.match(/.{1,2}/g);
+           
+            if (str.length == 2) {
+                var image1 = "<img class='image' src='" + IMAGE_PATH + str[0] + IMAGE_EXTENSION + "'>";
+                var image2 = "<img class='image' src='" + IMAGE_PATH + str[1] + IMAGE_EXTENSION + "'>";
 
-            str.forEach(function(letter) {
-                image = "<img src='" + IMAGE_PATH + letter + IMAGE_EXTENSION + "'>";
-                $('#translatedContent').append(image);
-            })
+                var word = $('#wordWrapper' + wordCounter).append("<div class='syllable'>" + image1 + image2 + "</div>");
+
+                $('#translatedContent').append(word);
+            } else {
+                str.forEach(function(letter) {
+                    image = "<img class='image' src='" + IMAGE_PATH + letter + IMAGE_EXTENSION + "'>";
+
+                    var word = $('#wordWrapper' + wordCounter).append("<div class='syllable'>" + image + "</div>");
+
+                    $('#translatedContent').append(word);
+                });
+            }
         }
     }
 
@@ -107,21 +123,48 @@ $(function() {
     }
 
     function generateImageForSyllable() {
-        var images = [];
-        var x = 0;
+        if ($('#landscape').is(':checked')) {
+            var images = [];
+            var x = 0;
 
-        $('#translatedContent img').each(function() {   
-            var image = {};
-            image.src = this.src;
-            image.x = x;
-            images.push(image);
+            $('.landscape').each(function() {
+                $(this).find('img').each(function() {
+                    var image = {};
+                    image.src = this.src;
+                    image.x = x;
+                    images.push(image);
 
-            x += 300;
-        });
+                    x += 300;
+                });
 
-        mergeImages(images, {
-            width: x,
-            height: 350
-        }).then(b64 => $('#output').attr('src', b64));
+                x += 300;
+            });
+
+            mergeImages(images, {
+                width: x,
+                height: 350
+            }).then(b64 => $('#output').attr('src', b64));
+        } else if ($('#portrait').is(':checked')) {
+            var images = [];
+            var x = 0;
+
+            $('.portrait').each(function() {
+                $(this).find('img').each(function() {
+                    var image = {};
+                    image.src = this.src;
+                    image.x = x;
+                    images.push(image);
+
+                    x += 300;
+                });
+
+                x += 300;
+            });
+
+            mergeImages(images, {
+                width: x,
+                height: 350
+            }).then(b64 => $('#output').attr('src', b64));
+        }
     }
 });
